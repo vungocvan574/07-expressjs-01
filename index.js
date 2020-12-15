71a8a10f597fac7cc64212cf2b5df8c48bb24b7c
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-15 22:37:03 +0000

    id view

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var { render } = require('pug');
-var app = express();
-var port = 3000;
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
+var shortId = require('shortid');
+var app = express();
+var port = 3000;
 var adapter = new FileSync('db.json');
 
 var db = low(adapter);
@@ -17,11 +18,6 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-var users = [
-    { id: 1, name: 'Đặng Xuân Hợi' },
-    { id: 2, name: 'Nguyễn Ngọc Hiệp' }
-];
-
 app.get('/', function(req, res) {
     res.render('index', {
         name: "VNV"
@@ -30,12 +26,13 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
     res.render('users/index', {
-        users: users
+        users: db.get('users').value()
     });
 });
 
 app.get('/users/search', function(req, res) {
     var q = req.query.q;
+    var users = db.get('users').value();
     var matchedUsers = users.filter(function(user) {
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
@@ -45,12 +42,23 @@ app.get('/users/search', function(req, res) {
     });
 });
 
+app.get('/users/:id', function(req, res) {
+    var id = parseInt(req.params.id);
+
+    var user = db.get('users').find({ id: id }).value();
+
+    res.render('users/view', {
+        user: user
+    });
+})
+
 app.get('/users/create', function(req, res) {
     res.render('users/create');
 });
 
 app.post('/users/create', function(req, res) {
-    users.push(req.body);
+    req.body.id = shortId.generate();
+    db.get('users').push(req.body).write();
     res.redirect('/users');
 })
 
