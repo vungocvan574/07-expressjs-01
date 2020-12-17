b6d221efb7e7886d4d80469e43185d3f8791f079
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-17 20:38:41 +0000

    middleware, validation seperated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -36,23 +36,7 @@ module.exports.userDetail = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
     req.body.id = shortId.generate();
-    var errors = [];
 
-    if (!req.body.name) {
-        errors.push("Name is required");
-    };
-
-    if (!req.body.phone) {
-        errors.push("Phone is required");
-    };
-
-    if (errors.length > 0) {
-        res.render('users/create', {
-            errors: errors,
-            values: req.body
-        });
-    } else {
-        db.get('users').push(req.body).write();
-        res.redirect('');
-    }
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,7 @@ var express = require('express');
 // var db = require('../db');
 //include controller
 var controller = require('../controller/user.controller')
+var validate = require('../validate/user.validate');
 
 var router = express.Router();
 
@@ -15,6 +16,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.userDetail);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
