6321c517de741e0e53082b370cb57f07b1fa0338
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-17 21:33:34 +0000

    cookie example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //req.query
 var express = require('express');
+var cookieParser = require('cookie-parser')
 
 var port = 3000;
 
@@ -12,6 +13,8 @@ app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser())
 
 app.use(express.static('./public'))
 
