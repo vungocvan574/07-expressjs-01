5cfc5694dbd5e11fbd223c5f7eec18e9ba75122a
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-18 00:17:13 +0000

    update md5 password in db

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,12 +1,14 @@
 var db = require('../db');
 
+var md5 = require('md5');
+
 module.exports.login = function(req, res) {
     res.render('auth/login')
 };
 
 module.exports.postLogin = function(req, res) {
     var email = req.body.email;
-    var password = req.body.password;
+    var password = req.body.password; //
     var user = db.get('users').find({ email: email }).value();
 
     if (!user) {
@@ -17,7 +19,9 @@ module.exports.postLogin = function(req, res) {
         return;
     }
 
-    if (user.password !== password) {
+    var hashedPassword = md5(password);
+
+    if (user.password !== hashedPassword) {
         res.render('auth/login', {
             errors: ["Wrong password."],
             values: req.body
